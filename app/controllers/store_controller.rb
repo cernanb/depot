299435194658a045271d5c7b55f30953141a3894
Cernan Bernardo <cernanb@gmail.com>
2018-01-06 13:32:26 -0800

    initial commit

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,11 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+
+    if session[:counter].nil?
+      @counter = session[:counter] = 1
+    else
+      @counter = session[:counter] += 1 
+    end
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,9 +4,16 @@ class Product < ApplicationRecord
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG or PNG image.'}
 
-#   validates ​:image_url​, ​allow_blank: ​​true​, ​format: ​{
-# ​ 	  ​with:    ​​%r{​​\.​​(gif|jpg|png)​​\Z​​}i​,
-# ​ 	  ​message: ​​'must be a URL for GIF, JPG or PNG image.'​
-# ​ 	}
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_line_items
+
+  private
+    def ensure_not_referenced_by_line_items
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw(:abort)
+      end
+    end
 
 end
